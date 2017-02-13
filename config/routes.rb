f85cc5013a21122a5f4c4b758b6f0a7a050df9e3
Dailey, Jon <jdailey@ICF2022885.local>
2017-02-13 16:09:35 -0500

    Fixed some routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :games, only: [:create, :new, :update]
+  resources :games, only: [:create, :new, :update, :destroy]
   get '/high_scores' => 'games#index', :as => :high_scores
   get '/games/:id/continue' => 'games#edit', :as => :continue_game
 
